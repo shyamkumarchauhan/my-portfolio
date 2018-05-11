0cc8f6f8b6281a3365db5f5073dd6fe78d751f1d
Shyamkumar Chauhan <shyamkumar.chauhan@gmail.com>
2018-05-11 16:23:52 +0530

    update python function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -32,7 +32,8 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
 
